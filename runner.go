c396ddc1e1e43e0e7b2c1246f1cae688f60b049f
Paulo Lobato <paulo@meliuz.com.br>
2018-03-21 20:44:20 -0400

    Add cmd.Wait() to waits the command process finish

--- runner.go
+++ runner.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 
 	log "github.com/sirupsen/logrus"
 )
+
+func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(d)
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
+	}
+	// never reached
+	panic(true)
+	return nil, nil
+}
 
 func RunCommand(command string, args []string, envVars []string) {
 	log.WithFields(log.Fields{
@@ -14,10 +42,27 @@ func RunCommand(command string, args []string, envVars []string) {
 	).Info("Running command")
 	cmd := exec.Command(command, args...)
 	cmd.Env = envVars
-	out, err := cmd.Output()
+	var stdout, stderr []byte
+	var errStdout, errStderr error
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	cmd.Start()
+
+	go func() {
+		stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn)
+	}()
+
+	go func() {
+		stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
+	}()
+
+	err := cmd.Wait()
 	if err != nil {
-		log.Error(err)
-	} else {
-		fmt.Printf("%s\n", out)
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatalf("failed to capture stdout or stderr\n")
+	}
+	outStr, errStr := string(stdout), string(stderr)
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
 }
